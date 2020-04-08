aacbec9b58afbfb421e4407529aa30c6c40ad045
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-04-08 15:39:05 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -0,0 +1,18 @@
+package com.codeclan.example.WhiskyTracker.repositories;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+
+//    Get all the whiskies for a particular year
+
+    List<Whisky> findByYear(Integer year);
+
+//    Get all the whisky from a particular distillery that's a specific age
+
+    List<Whisky> findWhiskiesByDistilleryNameAndAge(String name, Integer age);
+
+}
